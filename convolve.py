d5526b7e240504db059176c9b1d8c209798eff47
Krishna Suresh <krishna4132@gmail.com>
2021-10-21 23:03:34 -0400

    trying to get sobel to work

--- convolve.py
+++ convolve.py
@@ -1,15 +1,16 @@
 # bring in image
-# generate kernal
+# generate kernel
 # convolve
 # display input image and output
 
-from PIL import Image, ImageDraw
+from typing import cast
+from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 from math import sqrt, pi, exp
 
 def gauseDist(x,mu,sigma):
     return ( 2.*np.pi*sigma**2. )**-.5 * np.exp( -.5 * (x-mu)**2. / sigma**2. )
-def generateGaussianKernal(size=5, sigma=1):
+def generateGaussiankernel(size=5, sigma=1):
     kern = np.zeros((size,size))
     for i, row in enumerate(kern):
         for j, val in enumerate(row):
@@ -17,25 +18,33 @@ def generateGaussianKernal(size=5, sigma=1):
             kern[i,j] = gauseDist(dist, 0, sigma)
     return kern/kern.sum()
 
+def generateSobel():
+    return np.array([[-1,0,1],[-2, 0, 2],[-1, 0, 1]])
+
 def convolve(img, kern):
+    size = int((kern.shape[1]-1)/2)
     for i, row in enumerate(img):
         for j, val in enumerate(row):
-            if j>2 and j<(img.shape[0]-2) and i>2 and i<(img.shape[1]-2):
-                selected_reg = img[i-2:i+3, j-2:j+3]
-                img[i,j] = np.multiply(selected_reg, kern).sum()
+            if j>size and j<(img.shape[0]-size) and i>size and i<(img.shape[1]-size):
+                selected_reg = img[i-size:(i+size+1), j-size:(j+size+1)]
+                num = np.multiply(kern,selected_reg).sum()
+                if not (num ==num):
+                    print(selected_reg)
+                img[i,j] = num
     return img
 
-input = Image.open("lowres.jpg")
-img_arr = np.array(input)
-print(img_arr.shape[0:2])
-gray = img_arr[:,:,0]*0.2989 + img_arr[:,:,1]*0.5870 + img_arr[:,:,2]*0.1140
-PIL_image = Image.fromarray(gray.astype('uint8'), 'L')
-PIL_image.show()
-kernal = generateGaussianKernal(5, 10)
-blur = convolve(gray, kernal)
-
-print (kernal.sum())
-PIL_image = Image.fromarray(blur.astype('uint8'), 'L')
+input = Image.open("pineapple.jpg")
+gray = np.array(ImageOps.grayscale(input))
+print(gray.shape[0:2])
+# gray = img_arr[:,:,0]*0.2989 + img_arr[:,:,1]*0.5870 + img_arr[:,:,2]*0.1140
+# PIL_image = Image.fromarray(gray.astype('uint8'), 'L')
+# PIL_image.show()
+kernel = generateGaussiankernel(5, 10)
+blur = convolve(gray, kernel)
+kernel = generateSobel()
+x_sobel = convolve(blur, kernel)
+x_sobel = x_sobel-(x_sobel<200)
+PIL_image = Image.fromarray(x_sobel.astype('uint8'), 'L')
 PIL_image.show()
 # # input.show()
 
